17b71301e1b5510fc7445d0a00ac1b41df7e9332
tri27pham <tri.pham2002@outlook.com>
2024-07-10 13:47:50 +0100

    fixed focus

--- travel_app/lib/route.dart
+++ travel_app/lib/route.dart
@@ -25,7 +25,22 @@ class _RoutePlannerState extends State<RoutePlanner> {
   var uuid = Uuid();
 
   final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _mapSearchFocusNode = FocusNode();
+  final FocusNode _startLocationFocusNode = FocusNode();
+  final FocusNode _endLocationFocusNode = FocusNode();
+
+  bool _isEditingSearchLocation = false;
+  bool _isEditingStartLocation = false;
+  bool _isEditingEndLocation = false;
+
+  bool showStart = true;
+  bool showEnd = true;
+
   List<dynamic> places = [];
+
+  final TextEditingController _startLocationTextEditingController =
+      TextEditingController();
 
   static const CameraPosition initPos =
       CameraPosition(target: LatLng(51.5131, 0.1174), zoom: 14);
@@ -33,7 +48,9 @@ class _RoutePlannerState extends State<RoutePlanner> {
   final List<Marker> myMarker = [];
   bool _isDisposed = false;
 
-  double _containerHeight = 200;
+  double _containerHeight = 250;
+
+  bool createRoute = false;
 
   Future<Position> getUserLocation() async {
     await Geolocator.requestPermission()
@@ -104,13 +121,33 @@ class _RoutePlannerState extends State<RoutePlanner> {
   }
 
   void updateContainerHeight() {
-    if (_textEditingController.text.isEmpty) {
+    if (_isEditingSearchLocation ||
+        _isEditingStartLocation ||
+        _isEditingEndLocation) {
       setState(() {
-        _containerHeight = 200;
+        _containerHeight = 400;
       });
     } else {
       setState(() {
-        _containerHeight = 400;
+        _containerHeight = 250;
+      });
+    }
+  }
+
+  updateStartEndSearch() {
+    if (_isEditingStartLocation) {
+      setState(() {
+        showEnd = false;
+      });
+    } else if (_isEditingEndLocation) {
+      setState(() {
+        showStart = false;
+      });
+    }
+    if (!_isEditingStartLocation && !_isEditingEndLocation) {
+      setState(() {
+        showStart = true;
+        showEnd = true;
       });
     }
   }
@@ -157,6 +194,7 @@ class _RoutePlannerState extends State<RoutePlanner> {
                         height: 50,
                         child: TextFormField(
                           controller: _textEditingController,
+                          focusNode: _mapSearchFocusNode,
                           decoration: InputDecoration(
                               hintText:
                                   'Search for a location or search by image',
@@ -235,11 +273,271 @@ class _RoutePlannerState extends State<RoutePlanner> {
     );
   }
 
+  Widget StartLocationSearchBarWidget() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 2, 0, 2),
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: _textEditingController.text.isEmpty ? 350 : 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color:
+                        Color.fromARGB(255, 240, 240, 240), // Background color
+                    borderRadius: BorderRadius.circular(20), // Rounded corners
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                        width: 65,
+                        child: Text('Start'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 5, 0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Container(
+                        width: 190,
+                        height: 50,
+                        child: TextFormField(
+                          controller: _textEditingController,
+                          focusNode: _startLocationFocusNode,
+                          decoration: InputDecoration(
+                              hintText: 'Tap to search',
+                              hintStyle: TextStyle(
+                                  color: Colors.grey, // Set the hint text color
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child: Container(
+                          width: 40,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.my_location_rounded,
+                                size: 20,
+                                color: Colors.grey,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.transparent,
+                                  shadowColor: Colors.transparent,
+                                  padding: EdgeInsets.all(0))),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _textEditingController.text.isNotEmpty
+                    ? Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Container(
+                          width: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _startLocationTextEditingController.text = '';
+                            },
+                            child: Icon(Icons.arrow_back_sharp),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.grey[900],
+                                onPrimary: Colors.white,
+                                padding: EdgeInsets.zero,
+                                minimumSize: Size(50, 40)),
+                          ),
+                        ))
+                    : Container(),
+              ],
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              height: _textEditingController.text.isNotEmpty
+                  ? _containerHeight / 1.75
+                  : 0,
+              width: MediaQuery.of(context).size.width * 0.85,
+              padding: EdgeInsets.fromLTRB(5, 0, 5, 20),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: ListView.builder(
+                  itemCount: places.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.transparent,
+                      height: MediaQuery.of(context).size.height *
+                          0.06, // Specify the desired height
+                      width:
+                          MediaQuery.of(context).size.width, // Use full width
+                      child: ListTile(
+                        onTap: () async {
+                          List<Location> locations = await locationFromAddress(
+                              places[index]['description']);
+                          print(locations.last.longitude);
+                          print(locations.last.latitude);
+                        },
+                        leading: Icon(Icons.location_on),
+                        title: Text(
+                          places[index]['description'],
+                          overflow: TextOverflow
+                              .ellipsis, // Use ellipsis to indicate truncation
+                          maxLines: 1,
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget EndLocationSearchBarWidget() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 2, 0, 2),
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: _textEditingController.text.isEmpty ? 350 : 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color:
+                        Color.fromARGB(255, 240, 240, 240), // Background color
+                    borderRadius: BorderRadius.circular(20), // Rounded corners
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                        width: 65,
+                        child: Text('End'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 5, 0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Container(
+                        width: 190,
+                        height: 50,
+                        child: TextFormField(
+                          controller: _textEditingController,
+                          focusNode: _endLocationFocusNode,
+                          decoration: InputDecoration(
+                              hintText: 'Tap to search',
+                              hintStyle: TextStyle(
+                                  color: Colors.grey, // Set the hint text color
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child: Container(
+                          width: 40,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.my_location_rounded,
+                                size: 20,
+                                color: Colors.grey,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.transparent,
+                                  shadowColor: Colors.transparent,
+                                  padding: EdgeInsets.all(0))),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _textEditingController.text.isNotEmpty
+                    ? Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Container(
+                          width: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _startLocationTextEditingController.text = '';
+                            },
+                            child: Icon(Icons.arrow_back_sharp),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.grey[900],
+                                onPrimary: Colors.white,
+                                padding: EdgeInsets.zero,
+                                minimumSize: Size(50, 40)),
+                          ),
+                        ))
+                    : Container(),
+              ],
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              height: _textEditingController.text.isNotEmpty
+                  ? _containerHeight / 1.75
+                  : 0,
+              width: MediaQuery.of(context).size.width * 0.85,
+              padding: EdgeInsets.fromLTRB(5, 0, 5, 20),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: ListView.builder(
+                  itemCount: places.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.transparent,
+                      height: MediaQuery.of(context).size.height *
+                          0.06, // Specify the desired height
+                      width:
+                          MediaQuery.of(context).size.width, // Use full width
+                      child: ListTile(
+                        onTap: () async {
+                          List<Location> locations = await locationFromAddress(
+                              places[index]['description']);
+                          print(locations.last.longitude);
+                          print(locations.last.latitude);
+                        },
+                        leading: Icon(Icons.location_on),
+                        title: Text(
+                          places[index]['description'],
+                          overflow: TextOverflow
+                              .ellipsis, // Use ellipsis to indicate truncation
+                          maxLines: 1,
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget CreateRouteTitleAndButtonWidget() {
     return Padding(
       padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
       child: Padding(
-          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -251,15 +549,51 @@ class _RoutePlannerState extends State<RoutePlanner> {
               ),
               Container(
                   height: 45,
-                  // width: 60,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.add, color: Colors.white, size: 35),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 78, 199, 82)),
-                  )),
+                  width: 70,
+                  child: createRoute
+                      ? ElevatedButton(
+                          onPressed: () {
+                            _textEditingController.text = '';
+                            setState(() {
+                              createRoute = false;
+                            });
+                          },
+                          child: Icon(Icons.arrow_back_sharp),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.grey[900],
+                              onPrimary: Colors.white,
+                              padding: EdgeInsets.zero,
+                              minimumSize: Size(50, 40)),
+                        )
+                      : ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              createRoute = true;
+                            });
+                          },
+                          child: Icon(Icons.add, color: Colors.white, size: 35),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 78, 199, 82),
+                            padding: EdgeInsets.zero,
+                          ))),
             ],
           )),
+    );
+  }
+
+  Widget CreateRouteLocationSearchWidget() {
+    return Column(
+      children: [
+        CreateRouteTitleAndButtonWidget(),
+        showStart ? StartLocationSearchBarWidget() : Container(),
+        showEnd ? EndLocationSearchBarWidget() : Container(),
+      ],
+    );
+  }
+
+  Widget CreateRouteInitialWidget() {
+    return Column(
+      children: [CreateRouteTitleAndButtonWidget(), SearchBarWidget()],
     );
   }
 
@@ -287,18 +621,35 @@ class _RoutePlannerState extends State<RoutePlanner> {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [CreateRouteTitleAndButtonWidget(), SearchBarWidget()],
+          children: [
+            createRoute
+                ? CreateRouteLocationSearchWidget()
+                : CreateRouteInitialWidget()
+          ],
         ),
       ),
     );
   }
 
+  void _onFocusChange() {
+    setState(() {
+      _isEditingSearchLocation = _mapSearchFocusNode.hasFocus;
+      _isEditingStartLocation = _startLocationFocusNode.hasFocus;
+      _isEditingEndLocation = _endLocationFocusNode.hasFocus;
+      updateContainerHeight();
+      updateStartEndSearch();
+      print(showStart);
+      print(showEnd);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    _textEditingController.addListener(() {
-      updateContainerHeight();
-    });
+    _mapSearchFocusNode.addListener(_onFocusChange);
+    _startLocationFocusNode.addListener(_onFocusChange);
+    _endLocationFocusNode.addListener(_onFocusChange);
+
     _textEditingController.addListener(() {
       onModify();
     });
@@ -314,8 +665,13 @@ class _RoutePlannerState extends State<RoutePlanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [MapWidget(), CreateRouteWidget()],
+        body: GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Stack(
+        children: [MapWidget(), CreateRouteWidget()],
+      ),
     ));
   }
 }
