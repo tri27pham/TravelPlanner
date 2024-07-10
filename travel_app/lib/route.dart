caeccd3c46493e29713ac580b3f449573add951e
tri27pham <tri.pham2002@outlook.com>
2024-07-10 11:01:21 +0100

    add back button to search maps

--- travel_app/lib/route.dart
+++ travel_app/lib/route.dart
@@ -127,72 +127,98 @@ class _RoutePlannerState extends State<RoutePlanner> {
   }
 
   Widget SearchBarWidget() {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-            width: 350,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 240, 240, 240), // Background color
-              borderRadius: BorderRadius.circular(25), // Rounded corners
-            ),
-            child: Row(
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 5, 0),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                ),
                 Container(
-                  width: 300,
+                  width: _textEditingController.text.isEmpty ? 350 : 300,
                   height: 50,
-                  child: TextFormField(
-                    controller: _textEditingController,
-                    decoration: InputDecoration(
-                        hintText: 'Search for a location or search by image',
-                        hintStyle: TextStyle(
-                            color: Colors.grey, // Set the hint text color
-                            fontWeight: FontWeight.w300,
-                            fontSize: 13),
-                        border: InputBorder.none),
+                  decoration: BoxDecoration(
+                    color:
+                        Color.fromARGB(255, 240, 240, 240), // Background color
+                    borderRadius: BorderRadius.circular(25), // Rounded corners
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 5, 0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 50,
+                        child: TextFormField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              hintText:
+                                  'Search for a location or search by image',
+                              hintStyle: TextStyle(
+                                  color: Colors.grey, // Set the hint text color
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                //
-
-                //
+                _textEditingController.text.isNotEmpty
+                    ? Padding(
+                        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Container(
+                          width: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _textEditingController.text = '';
+                            },
+                            child: Icon(Icons.arrow_back_sharp),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.grey[900],
+                                onPrimary: Colors.white,
+                                padding: EdgeInsets.zero,
+                                minimumSize: Size(50, 40)),
+                          ),
+                        ))
+                    : Container(),
               ],
             ),
-          ),
-          AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            height: _textEditingController.text.isNotEmpty
-                ? _containerHeight / 3
-                : 0,
-            width: MediaQuery.of(context).size.width * 0.75,
-            child: ListView.builder(
-                itemCount: places.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: Colors.white,
-                    height: MediaQuery.of(context).size.height *
-                        0.05, // Specify the desired height
-                    width: MediaQuery.of(context).size.width, // Use full width
-                    child: ListTile(
-                      onTap: () async {
-                        List<Location> locations = await locationFromAddress(
-                            places[index]['description']);
-                        print(locations.last.longitude);
-                        print(locations.last.latitude);
-                      },
-                      title: Text(places[index]['description']),
-                    ),
-                  );
-                }),
-          ),
-        ],
+            AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              height: _textEditingController.text.isNotEmpty
+                  ? _containerHeight / 3
+                  : 0,
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: ListView.builder(
+                  itemCount: places.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.white,
+                      height: MediaQuery.of(context).size.height *
+                          0.05, // Specify the desired height
+                      width:
+                          MediaQuery.of(context).size.width, // Use full width
+                      child: ListTile(
+                        onTap: () async {
+                          List<Location> locations = await locationFromAddress(
+                              places[index]['description']);
+                          print(locations.last.longitude);
+                          print(locations.last.latitude);
+                        },
+                        title: Text(places[index]['description']),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
