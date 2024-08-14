ac9d9f7b1d89c68ba5232ae2896a9447a5b7f558
tri27pham <tri.pham2002@outlook.com>
2024-08-14 13:52:20 +0100

    dream list done, need to view individual locaions once added

--- travel_app/lib/models/predicted_route_place_model.dart
+++ travel_app/lib/models/predicted_route_place_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:developer';
 
 class PredictedRoutePlace {
   final String description;
@@ -8,6 +9,7 @@ class PredictedRoutePlace {
   PredictedRoutePlace({required this.description});
 
   factory PredictedRoutePlace.fromJson(Map<String, dynamic> json) {
+    log(json.toString());
     return PredictedRoutePlace(description: json['description']);
   }
 }
